b0cd47890c5cc44cf78b852df707c4c16a774be0
Jahid (Zahid) <68467966+git-zaidism@users.noreply.github.com>
2023-01-05 15:00:39 +0530

    Create ReverseNumber.java
    This code prompts the user to enter a number, and then uses a loop to reverse the digits of the number. The reverse method first initializes the reversedNum variable to 0. It then enters a loop which continues until num is 0. Inside the loop, the reversedNum variable is updated to be equal to itself multiplied by 10 plus the last digit of num. The num variable is then updated to be equal to itself divided by 10. When the loop ends, the reversedNum variable contains the reversed number.

---
+++ ReverseNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int reversedNum = reverse(num);
+        System.out.println("Reversed number: " + reversedNum);
+    }
+
+    private static int reverse(int num) {
+        int reversedNum = 0;
+        while (num != 0) {
+            reversedNum = reversedNum * 10 + num % 10;
+            num /= 10;
+        }
+        return reversedNum;
+    }
+}
